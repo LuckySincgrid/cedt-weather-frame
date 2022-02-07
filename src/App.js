f5ed99f4f021fd6046e27911b637514d5e112bb7
C-Y-P-H-E-R <quasarkid123@gmail.com>
2022-02-07 14:49:59 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useEffect , useState} from 'react';
 import './App.css';
 
 function App() {
+  const API_KEY = "89c13ea938871f343abc1e564db2d8f9"
+  const lat = "28.6667"
+  const longt = "77.2167"
+  const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${longt}&appid=${API_KEY}`
+
+  const [weatherdataJSON, setWeatherData] = useState({});
+  
+  // useEffect(() => {
+  //   // fetch(BASE_URL)
+  //   // .then(Response => Response.json())
+  //   // .then(data => setWeatherData(data.main))
+
+  //   // const timer = setTimeout(() => {
+  //   //   // fetchData();
+  //   //   console.log("Just to delay the api calls")
+  //   // }, 5000);
+
+
+  //   // console.log(weatherdataJSON)
+  //   // return () => clearTimeout(timer);
+  // }, [/*weatherdataJSON*/]) 
+
+  useEffect(() => {
+    
+
+    const timer = setTimeout(() => {
+      // fetchData();
+      fetch(BASE_URL)
+      .then(Response => Response.json())
+      .then(data => setWeatherData(data.main))
+      console.log("Just to delay the api calls")
+    }, 5000);
+
+
+    console.log(weatherdataJSON)
+    return () => clearTimeout(timer);
+  }, [weatherdataJSON]) 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App_info">
+        <h2 className="Temp_text">Min. Temperature: {weatherdataJSON.temp_min}</h2>
+        <h2 className="Temp_text">Max. Temperature: {weatherdataJSON.temp_max}</h2>
+        <h2 className="Temp_text">Humidity: {weatherdataJSON.humidity}</h2>
+      </div>
+      <div className="App_animation">
+         {/* this part will have animation */}
+         <iframe src="https://embed.lottiefiles.com/animation/4800"></iframe>
+      </div>  
     </div>
   );
 }
 
 export default App;
+
+// {weatherdataJSON.temp_min}
+// {weatherdataJSON.temp_max}
+// {weatherdataJSON.humidity}
