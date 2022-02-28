b92284894d9fafd20de6d5316df5de0c85b5d8e2
C-Y-P-H-E-R <quasarkid123@gmail.com>
2022-02-28 17:27:01 +0530

    pre-production-test-1

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import animationData from './lotties/partly_cloudy.json';
 
 
 function App() {
-  const API_KEY = "89c13ea938871f343abc1e564db2d8f9"
-  const lat = "28.6667"
-  const longt = "77.2167"
-  const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${longt}&appid=${API_KEY}`
+  // const API_KEY = "89c13ea938871f343abc1e564db2d8f9"
+  // const lat = "28.6667"
+  // const longt = "77.2167"
+  const BASE_URL = `http://server2.sincgrid.com:5000/api/v1/cedt/weather-station-221`
 
   const [weatherdataJSON, setWeatherData] = useState({});
-
-
+  const [dataArray, setDataArray] = useState([]);
+// 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      fetch(BASE_URL)
+    const timer = setTimeout(async () => {
+      await fetch(BASE_URL)
       .then(Response => Response.json())
-      .then(data => setWeatherData(data.main))
+      .then(data => setWeatherData(data.data))
+      
+      // setDataArray()
+      setDataArray(Object.entries(weatherdataJSON).map(e => e[1]))
+      // Object.entries(data.data).map(e => e[1])
+
       console.log("Just to delay the api calls")
-    }, 3000);
+    }, 1500);
 
 
-    console.log(weatherdataJSON)
+    console.log(dataArray)
     return () => clearTimeout(timer);
   }, [weatherdataJSON]) 
 
@@ -34,21 +39,32 @@ function App() {
       preserveAspectRatio: "xMidYMid slice"
     }
   };
+
+  // let setter = false
+  let val = 0;
   
   return (
     <div className="App">
-      <div className="App_info">
-        <h2 className="Temp_text">Min. Temperature: {weatherdataJSON.temp_min}&deg;</h2>
-        <h2 className="Temp_text">Max. Temperature: {weatherdataJSON.temp_max}&deg;</h2>
-        <h2 className="Temp_text">Humidity: {weatherdataJSON.humidity}%</h2>
+      {
+        dataArray.length !== 0 && (
+        <div className="wrapper">
+        <div className="App_info">
+          {/* <h2 className="Temp_text">Altitude: {dataArray[val]?.iotasync_data?.alt}</h2> */}
+            <h2 className="Temp_text">Temperature: {dataArray[val]?.iotasync_data?.temp}&deg;</h2>
+            <h2 className="Temp_text">Pressure: {dataArray[val]?.iotasync_data?.pressure}</h2>
+            <h2 className="Temp_text">Humidity: {dataArray[val]?.iotasync_data?.humidity}%</h2>
+          </div>
+          <div className="App_animation">
+            <Lottie 
+                options={defaultOptions}
+                  height={40}
+                  width={40}
+          />
+        </div>  
       </div>
-      <div className="App_animation">
-         <Lottie 
-            options={defaultOptions}
-              height={50}
-              width={50}
-      />
-      </div>  
+      )
+      }
+
     </div>
   );
 }
