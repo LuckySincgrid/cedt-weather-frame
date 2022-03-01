51958b691d0527fd541b93e4b554b639bc2da503
C-Y-P-H-E-R <quasarkid123@gmail.com>
2022-03-01 15:26:27 +0530

    before-epoch-changes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ function App() {
   let val = 0;
   const [dataArray, setDataArray] = useState([]);
   // const [epochTime, setEpochTime] = useState();
-  const [tempState, setTempState] = useState((Math.random()*10 + 20).toPrecision(2));
-  const [pressureState, setPressureState] = useState(Math.floor((Math.random()*10 + 972).toFixed(3)));
-  const [humidityState, setHumidityState] = useState((Math.random()*10 + 42).toPrecision(2));
+  const [tempState, setTempState] = useState('Loading..');
+  const [pressureState, setPressureState] = useState('Loading..');
+  const [humidityState, setHumidityState] = useState('Loading..');
+
+  // (Math.random()*10 + 20).toPrecision(2)
+  // Math.floor((Math.random()*10 + 972).toFixed(3))
+  // (Math.random()*10 + 42).toPrecision(2)
 
   useEffect(() => {
 
@@ -59,9 +63,9 @@ function App() {
           <div className="wrapper">
           <div className="App_info">
             {/* <h2 className="Temp_text">Altitude: {dataArray[val]?.iotasync_data?.alt}</h2> */}
-              <h2 className="Temp_text">Temperature: {tempState}&deg;C</h2>
-              <h2 className="Temp_text">Pressure: {pressureState} hPa</h2>
-              <h2 className="Temp_text">Humidity: {humidityState}%</h2>
+              <h2 className="Temp_text">Temperature: {(tempState === "") ? 'Loading..': (`${tempState}&deg;C`) }</h2>
+              <h2 className="Temp_text">Pressure: {(pressureState === "") ? 'Loading..': (`${pressureState} hPa`)} </h2>
+              <h2 className="Temp_text">Humidity: {(humidityState === "") ? 'Loading..': (`${humidityState}%`)}</h2>
             </div>
             <div className="App_animation">
             <div className="status_dot status_dot_active"></div>
