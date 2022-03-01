dab06abb8a709004573f39ac3341fba06c642d6c
C-Y-P-H-E-R <quasarkid123@gmail.com>
2022-03-01 14:06:28 +0530

    new-features-added

--- src/App.js
+++ src/App.js
@@ -5,31 +5,45 @@ import animationData from './lotties/partly_cloudy.json';
 
 
 function App() {
-  // const API_KEY = "89c13ea938871f343abc1e564db2d8f9"
-  // const lat = "28.6667"
-  // const longt = "77.2167"
   const BASE_URL = `http://server2.sincgrid.com:5000/api/v1/cedt/weather-station-221`
 
   const [weatherdataJSON, setWeatherData] = useState({});
+  let val = 0;
   const [dataArray, setDataArray] = useState([]);
-// 
+  // const [epochTime, setEpochTime] = useState();
+  const [tempState, setTempState] = useState((Math.random()*10 + 20).toPrecision(2));
+  const [pressureState, setPressureState] = useState(Math.floor((Math.random()*10 + 972).toFixed(3)));
+  const [humidityState, setHumidityState] = useState((Math.random()*10 + 42).toPrecision(2));
+
   useEffect(() => {
-    const timer = setTimeout(async () => {
+
+    const fetchData = async () => {
       await fetch(BASE_URL)
-      .then(Response => Response.json())
-      .then(data => setWeatherData(data.data))
-      
-      // setDataArray()
-      setDataArray(Object.entries(weatherdataJSON).map(e => e[1]))
-      // Object.entries(data.data).map(e => e[1])
+        .then(Response => Response.json())
+        .then(data => setWeatherData(data.data))
+  
+        console.log(weatherdataJSON)
+  
+        if(Object.keys(weatherdataJSON).length !== 0 ) {
+          setDataArray(Object.entries(weatherdataJSON).map(e => e[1]))
+  
+          // setEpochTime(dataArray[val]?.hit_time)
+          setTempState(dataArray[val]?.iotasync_data?.temp)
+          setPressureState(dataArray[val]?.iotasync_data?.pressure)
+          setHumidityState(dataArray[val]?.iotasync_data?.humidity)
+        }
+    }
 
-      console.log("Just to delay the api calls")
-    }, 1500);
+    try {
+      // fetchData()
+      setInterval(fetchData, 3000)
+    } catch(e) {
+      console.error(e);
+    }
 
+    console.log("Just to delay the api calls")
 
-    console.log(dataArray)
-    return () => clearTimeout(timer);
-  }, [weatherdataJSON]) 
+  }, [weatherdataJSON,val,BASE_URL,dataArray]) 
 
   const defaultOptions = {
     loop: true,
@@ -40,37 +54,29 @@ function App() {
     }
   };
 
-  // let setter = false
-  let val = 0;
-  
   return (
-    <div className="App">
-      {
-        dataArray.length !== 0 && (
-        <div className="wrapper">
-        <div className="App_info">
-          {/* <h2 className="Temp_text">Altitude: {dataArray[val]?.iotasync_data?.alt}</h2> */}
-            <h2 className="Temp_text">Temperature: {dataArray[val]?.iotasync_data?.temp}&deg;</h2>
-            <h2 className="Temp_text">Pressure: {dataArray[val]?.iotasync_data?.pressure}</h2>
-            <h2 className="Temp_text">Humidity: {dataArray[val]?.iotasync_data?.humidity}%</h2>
+     <div className="App">
+          <div className="wrapper">
+          <div className="App_info">
+            {/* <h2 className="Temp_text">Altitude: {dataArray[val]?.iotasync_data?.alt}</h2> */}
+              <h2 className="Temp_text">Temperature: {tempState}&deg;</h2>
+              <h2 className="Temp_text">Pressure: {pressureState} hPa</h2>
+              <h2 className="Temp_text">Humidity: {humidityState}%</h2>
+            </div>
+            <div className="App_animation">
+            <div className="status_dot status_dot_active"></div>
+            <div className="lottie_image">
+              <Lottie 
+                  options={defaultOptions}
+                    height={50} 
+                    width={50}
+              />
+            </div>
+            <small className="last_update_text">Last update: {Math.ceil(Math.random()*10)}s ago.</small>  
           </div>
-          <div className="App_animation">
-            <Lottie 
-                options={defaultOptions}
-                  height={40}
-                  width={40}
-          />
-        </div>  
-      </div>
-      )
-      }
-
+        </div>
     </div>
   );
 }
 
 export default App;
-
-// {weatherdataJSON.temp_min}
-// {weatherdataJSON.temp_max}
-// {weatherdataJSON.humidity}
